9bc8d036eb2c967536836b1f20a1386855fb1f91
Logan Lindquist <logan@llbbl.com>
2022-06-19 17:01:29 -0500

    adding more aliases

--- macos_shell_aliases.sh
+++ macos_shell_aliases.sh
@@ -21,11 +21,20 @@ alias recent.week=\"find . -type d -mtime -7 -maxdepth 1 -mindepth 1\"
 # add string to the end of the file
 alias append=\"echo '\$1' >>\"
 
-# javascript aliases
-#
-# fix next cli
+# javascript
 alias next=\"npx next\"
 alias npm-list-globals=\"npm list -g --depth 0\"
+alias npm-check-globals=\"npm outdated -g --depth=0\"
+
+# alias npm-upgrade-globals='echo \"upgrade individual with: npm update -g <package_name>\"'
+alias npm-upgrade-globals='npm update -g'
+
+alias npm-list-dev-dependencies=\"npm list --depth=0 --dev\"
+
+alias npm-version=\"echo '[node version]' && nvm ls current && echo '[npm version]' && npm --version && echo '[where is npm]' && which npm\"
+
+# check npx is installed
+alias npx-check=\"npx --version\"
 
 # write out .nvmrc file
 alias nvmrc=\"node -v > .nvmrc\"
@@ -33,6 +42,26 @@ alias nvmrc=\"node -v > .nvmrc\"
 # python 3
 alias python='/opt/homebrew/bin/python3'
 alias pip='/opt/homebrew/bin/pip3'
+
+# php, composer, laravel
+alias php-version=\"echo '[php -v]' && php -v && echo '[where is php]' && ls -al /opt/homebrew/bin/php\"
+
+# list composer global packages
+alias composer-list-globals=\"composer global show\"
+
+# composer global update everything
+alias composer-global-update=\"composer global update\"
+
+# composer install global packages
+alias composer-global=\"composer global require\"
+
+# composer search global packages
+alias composer-search-global=\"composer-list-globals | grep\"
+
+# composer global remove a package
+alias composer-global-remove=\"composer global remove\"
+
+
 
 # ### ALIASES ###
 "
