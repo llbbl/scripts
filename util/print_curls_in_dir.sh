cab059f43da71d7c8335f57215601a86f6568915
Logan Lindquist <logan@llbbl.com>
2022-08-20 11:59:50 -0500

    cleanup

--- util/print_curls_in_dir.sh
+++ util/print_curls_in_dir.sh
@@ -9,9 +9,6 @@ find . -type f -print0 | while IFS= read -r -d $'\0' line; do
     # remove "./" from start of filename
     _line=$(echo $line | sed 's/\.\///g')
 
-    # remove any file extension from line
-    # no_ext=$(echo $_line | sed 's/\..*//g')
-
     echo "curl -sO $base_url$_line && chmod +x $line"
 
 done
